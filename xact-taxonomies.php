05eb24f67a485374f42fbc434716cff77777b1e2
Mohamed Najiub <m.najiub@ais-works.com>
2020-02-02 02:08:12 -0800

    add cmb2 file

--- xact-taxonomies.php
+++ xact-taxonomies.php
@@ -1,7 +1,7 @@
 <?php 
 function xact_widgets_init() {
 	
-	// register custom categories
+	// register custom categories for work
     register_taxonomy( 'work_categories', array('work'), array(
         'hierarchical' => true, // Set this to 'false' for non-hierarchical taxonomy (like tags)
         'labels' => array(
@@ -21,6 +21,28 @@ function xact_widgets_init() {
         'show_admin_column' => true,
         'query_var' => true,
         'rewrite' => array( 'slug' => 'projects_categories' )
+    ));
+
+    // register custom categories capabilities
+    register_taxonomy( 'capabilities_categories', array('capabilities'), array(
+        'hierarchical' => true, // Set this to 'false' for non-hierarchical taxonomy (like tags)
+        'labels' => array(
+            'name' => 'Categories',
+            'singular_name' => 'Category',
+            'search_items' => 'Search Capability Categories',
+            'all_items' => 'All Capability Categories',
+            'parent_item' => 'Parent Category',
+            'parent_item_colon' => 'Parent Category:',
+            'edit_item' => 'Edit Category',
+            'update_item' => 'Update Category',
+            'add_new_item' => 'Add New Project Category',
+            'new_item_name' => 'New Category Name',
+            'menu_name' => 'Categories',
+        ),
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'capabilities_categories' )
     ));
 }
 
