56a509a4941e9e5c0e4a5791c8d9bb99e6511081
Mohamed Najiub <m.najiub@ais-works.com>
2020-02-03 07:36:40 -0800

    add custom widgets

---
+++ xact-widgets.php
@@ -0,0 +1,17 @@
+<?php
+ 
+function xact_register_widgets() {
+    register_sidebar( array(
+		'name'          => 'Contacts Footer',
+		'id'            => 'contacts_footer',
+		'description'   => 'Add widgets here.',
+		'before_widget' => '<ul id="%1$s" class="p-0">',
+		'after_widget'  => '</ul>',
+		'before_title'  => '<h4 class="widget_title">',
+		'after_title'   => '</h4>',
+	) );
+}
+
+add_action( 'widgets_init', 'xact_register_widgets' );
+
+?>
