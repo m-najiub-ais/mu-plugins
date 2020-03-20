4f03fe4cec26a50f85c17458e80af5dc24709022
m-najiub-ais <m.najiub@ais-works.com>
2020-03-20 16:00:43 +0200

    add team Post Type

--- xact-post-types.php
+++ xact-post-types.php
@@ -16,9 +16,11 @@ function xact_post_types()
             'singular_name' => "Project"
         ),
         'menu_icon' => 'dashicons-category',
-        'has_archive' => true,
+        'has_archive' => false,
         'rewrite' => array('slug' => 'works'),
         'supports' => array('title', 'excerpt', 'thumbnail'),
+        // the post type doesn't have single page
+        'publicly_queryable' => false
     ));
 
     // our capabilities post type
@@ -32,9 +34,37 @@ function xact_post_types()
             'singular_name' => "Capability"
         ),
         'menu_icon' => 'dashicons-performance',
-        'has_archive' => true,
+        'has_archive' => false,
         'rewrite' => array('slug' => 'works'),
         'supports' => array('title', 'editor', 'thumbnail'),
+        // the post type doesn't have single page
+        'publicly_queryable' => false
+    ));
+
+    // our Team post type
+    register_post_type('team', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Team',
+            'new_item' => "Add Member",
+            'add_new' => "Add Member",
+            'add_new_item' => 'Add Member',
+            'edit_item' => 'Edit Member',
+            'all_items' => 'All Members',
+            'singular_name' => 'Team Member',
+            'search_items' => 'There is no team members had been added',
+            'featured_image' => 'Member Image',
+            'set_featured_image ' => 'Set Member Image',
+            'remove_featured_image  ' => 'Remover Member Image',
+            'use_featured_image' => 'Use as Member Image',
+            'item_updated' => 'Member Updated'
+        ),
+        'menu_icon' => 'dashicons-buddicons-buddypress-logo',
+        'has_archive' => false,
+        'rewrite' => array('slug' => 'works'),
+        'supports' => array('title', 'editor', 'thumbnail'),
+        // the post type doesn't have single page
+        'publicly_queryable' => false
     ));
 }
 
