31cbc9f7004e3752632e8f5e2ede880a1700a915
Mohamed Najiub <mohamednajiub@gmail.com>
2020-03-29 17:06:35 +0200

    edit capabilities grid

--- xact-shortcode.php
+++ xact-shortcode.php
@@ -45,22 +45,15 @@ function xact_shortcode_capabilities($atts)
     }
 ?>
     <section id="our-capabilities" class="container">
-        <?php
-        while ($loop->have_posts()) :
+        <div class="row">
+            <div class="col-lg-10 col-md-12">
+                <?php
+                while ($loop->have_posts()) :
 
-            $loop->the_post();
-        ?>
-            <div class="capability-container row pb-5">
-                <div class="col-lg-10 col-md-12">
-                    <div class="row">
+                    $loop->the_post();
+                ?>
+                    <div class="capability-container row pb-5">
                         <div class="col-md-6 image">
-                            <!-- <img src="<?php /*
-                    if (has_post_thumbnail()) {
-                    echo get_the_post_thumbnail_url('capabilities-thumb');
-                    } else {
-                    null;
-                    }
-                ?>" alt="<?php the_title(); */ ?>"> -->
                             <?php the_post_thumbnail('capabilities-thumb'); ?>
                         </div>
                         <div class="col-md-6 details">
@@ -70,12 +63,12 @@ function xact_shortcode_capabilities($atts)
                             ?>
                         </div>
                     </div>
-                </div>
+                <?php
+                endwhile;
+                wp_reset_postdata();
+                ?>
             </div>
-        <?php
-        endwhile;
-        wp_reset_postdata();
-        ?>
+        </div>
     </section>
 <?php
 }
@@ -115,112 +108,112 @@ function xact_shortcode_work($atts)
     }
 ?>
     <section id="our-work" class="pt-5 mt-5">
-      <!-- section title -->
-      <div class="header">
-        <div class="container">
-          <h2 class="gradient-work"><span class="font-light">Our</span> Work <?php echo $atts['slider-title'] ?></h2>
-        </div>      
-      </div>
-      <!-- slider section -->
-      <div class="slider pt-5">
-        <div class="our_work_slider">
-          <?php
+        <!-- section title -->
+        <div class="header">
+            <div class="container">
+                <h2 class="gradient-work"><span class="font-light">Our</span> Work <?php echo $atts['slider-title'] ?></h2>
+            </div>
+        </div>
+        <!-- slider section -->
+        <div class="slider pt-5">
+            <div class="our_work_slider">
+                <?php
+                while ($loop->have_posts()) :
+                    $loop->the_post();
+                ?>
+                    <!-- image slider -->
+                    <div class="img_slider" onclick="ToogleSlider(<?php the_ID() ?>)">
+                        <a class="<?php echo 'scroll_tag_' . get_the_ID() ?>">
+                            <div class="overlay"></div>
+                            <img class="img-slide" src=" <?php
+                                                            if (has_post_thumbnail()) {
+                                                                echo get_the_post_thumbnail_url();
+                                                            } else {
+                                                                null;
+                                                            }
+                                                            ?>" alt="<?php the_title(); ?>" id="1">
+                            <div class="img_text" id="<?php echo 'img_text_' . get_the_ID() ?>">
+                                <h2><?php the_title(); ?></h2>
+                                <p>
+                                    <?php
+                                    if (has_excerpt(get_the_id())) {
+                                        the_excerpt();
+                                    }
+                                    ?>
+                                </p>
+                                <div class="arrow">
+                                    <img src="<?php echo get_template_directory_uri() . '/images/right-chevron.svg'; ?>">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                <?php
+                endwhile;
+                ?>
+            </div>
+        </div>
+        <!-- End slider section -->
+
+        <!-- section details -->
+        <div class="content-wrapper">
+            <?php
             while ($loop->have_posts()) :
                 $loop->the_post();
-          ?>
-            <!-- image slider -->
-            <div class="img_slider" onclick="ToogleSlider(<?php the_ID() ?>)">
-              <a class="<?php echo 'scroll_tag_'.get_the_ID()?>">
-                <div class="overlay"></div>
-                <img class="img-slide" src=" <?php
-                  if (has_post_thumbnail()) {
-                    echo get_the_post_thumbnail_url();
-                  } else {
-                    null;
-                  }
-                ?>" alt="<?php the_title(); ?>" id="1">
-                <div class="img_text" id="<?php echo 'img_text_'.get_the_ID()?>">
-                  <h2><?php the_title();?></h2>
-                  <p>
-                    <?php
-                      if ( has_excerpt( get_the_id() ) ){
-                        the_excerpt();
-                      }
-                    ?>
-                  </p>
-                  <div class="arrow">
-                    <img src="<?php echo get_template_directory_uri() . '/images/right-chevron.svg'; ?>">
-                  </div>
-                </div>
-              </a>
-            </div>
-          <?php
-            endwhile;
-          ?>
-        </div>
-      </div>
-      <!-- End slider section -->
-
-      <!-- section details -->
-      <div class="content-wrapper">
-        <?php
-          while ($loop->have_posts()) :
-            $loop->the_post();
-            // get custom fields data
-            $project_details = get_post_meta( get_the_ID(), 'work_repeat_group', 1 );
-            // check if there is data in custom field
-            if($project_details):
-        ?>
-
-          <div class="container pb-5 our_work_content" id="<?php echo 'content_'.get_the_ID()?>">
-            <!-- close icon -->
-            <a href="#our-work">
-              <img src="<?php echo get_template_directory_uri().'/images/close.svg'?>" class="close-icon" alt="Phone icon">
-            </a>
-            <!-- end close icon -->
-
-            <!-- loop throught project details -->
-            <?php
-              foreach($project_details as $details):
+                // get custom fields data
+                $project_details = get_post_meta(get_the_ID(), 'work_repeat_group', 1);
+                // check if there is data in custom field
+                if ($project_details) :
             ?>
-            <div class="row pt-5 flex-md-row our_work_content_section">
-              <div class="col-md-10">
-                <div class="row">
-                  <div class="col-md-6 pt-3">
-                    <?php
-                      echo $details['project_description']
-                    ?>
+
+                    <div class="container pb-5 our_work_content" id="<?php echo 'content_' . get_the_ID() ?>">
+                        <!-- close icon -->
+                        <a href="#our-work">
+                            <img src="<?php echo get_template_directory_uri() . '/images/close.svg' ?>" class="close-icon" alt="Phone icon">
+                        </a>
+                        <!-- end close icon -->
+
+                        <!-- loop throught project details -->
+                        <?php
+                        foreach ($project_details as $details) :
+                        ?>
+                            <div class="row pt-5 flex-md-row our_work_content_section">
+                                <div class="col-md-10">
+                                    <div class="row">
+                                        <div class="col-md-6 pt-3">
+                                            <?php
+                                            echo $details['project_description']
+                                            ?>
+                                        </div>
+                                        <?php
+                                        // check if there is images
+                                        if ($details['project_images']) :
+                                        ?>
+                                            <div class="col-md-6 pt-3 img">
+                                                <?php
+                                                foreach ($details['project_images'] as $attachment_id => $attachment_url) :
+                                                    echo wp_get_attachment_image($attachment_id, 'medium', "", ["class" => "our_work_img"]);
+                                                endforeach;
+                                                ?>
+                                            </div>
+                                        <?php
+                                        endif; // check images check
+                                        ?>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <?php
+                        endforeach;
+                        ?>
                     </div>
-                    <?php
-                      // check if there is images
-                      if($details['project_images']):
-                    ?>
-                  <div class="col-md-6 pt-3 img">
-                    <?php
-                      foreach($details['project_images'] as $attachment_id => $attachment_url):
-                        echo wp_get_attachment_image( $attachment_id, 'medium', "", ["class" => "our_work_img"]  );
-                      endforeach;
-                    ?>
-                  </div>
-                  <?php
-                    endif; // check images check
-                  ?>
-                </div>
-              </div>
-            </div>
 
             <?php
-              endforeach;
+                endif; // end check of data in custom field
+            endwhile;
+            wp_reset_postdata();
             ?>
-          </div>
-
-        <?php
-            endif; // end check of data in custom field
-          endwhile;
-          wp_reset_postdata();
-        ?>
-      </div>
-      <!-- end section details -->
+        </div>
+        <!-- end section details -->
     </section>
 <?php
 }
